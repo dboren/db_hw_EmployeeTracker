35ca391f62572367614d7b5da1a6389f4388725e
dboren <danielboren@att.net>
2021-03-22 17:09:46 -0500

    added starting seed info for each table

--- seed.sql
+++ seed.sql
@@ -11,7 +11,7 @@ CREATE TABLE departments (
 CREATE TABLE roles (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DEC(10000,2),
+    salary DEC(100000,2),
     department_id INTEGER(11),
     PRIMARY KEY (id),
     FOREIGN KEY department_id REFERENCES departments(id)
@@ -25,4 +25,67 @@ CREATE TABLE employees (
     manager_id INTEGER(11),
     PRIMARY KEY (id),
     FOREIGN KEY role_id REFERENCES roles(id);
-)
+);
+
+
+INSERT INTO departments (name)
+VALUES ("customer service", "admin", "sales", "marketing", "hr");
+
+
+INSERT INTO roles (title, salary)
+VALUES ("csa", 30000);
+
+INSERT INTO roles (title, salary)
+VALUES ("csm", 35000);
+
+INSERT INTO roles (title, salary)
+VALUES ("CEO", 50000);
+
+INSERT INTO roles (title, salary)
+VALUES ("salesperson", 31000);
+
+INSERT INTO roles (title, salary)
+VALUES ("sales manager", 36000);
+
+INSERT INTO roles (title, salary)
+VALUES ("marketing director", 37000);
+
+INSERT INTO roles (title, salary)
+VALUES ("designer", 32000);
+
+INSERT INTO roles (title, salary)
+VALUES ("HR mananger", 38000);
+
+INSERT INTO roles (title, salary)
+VALUES ("HR specialist", 33000);
+
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Rusty", "Shackelford");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Michael", "Hansen");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Gemma", "Ray");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Isaac", "Giorno");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Joseph", "Shabadoo");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Lola", "Swinton");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Klaus", "Rooney");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Terri", "Visitor");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Edward", "Guy");
+
+INSERT INTO employees (first_name, last_name,)
+VALUES ("Boris", "Sisko");
