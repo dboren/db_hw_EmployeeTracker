ba637e6c81b471eca92d0ad07aceb679a4363be1
dboren <danielboren@att.net>
2021-03-23 19:40:09 -0500

    corrected schema.sql syntax in order to create tables without errors from the foreign key statements

--- schema.sql
+++ schema.sql
@@ -2,27 +2,36 @@ DROP DATABASE IF EXISTS workforce_db;
 
 CREATE DATABASE workforce_db;
 
+USE workforce_db;
+
 CREATE TABLE departments (
-    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    id INT(11) AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    id INT(11) AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DEC(100000,2),
+    salary DEC(65,2),
     department_id INTEGER(11),
     PRIMARY KEY (id),
-    FOREIGN KEY department_id REFERENCES departments(id)
+    INDEX par_ind (department_id),
+    CONSTRAINT fk_departments FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE employees (
-    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    id INT(11) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30),
-    role_id INTEGER(11) NOT NULL,
-    manager_id INTEGER(11),
+    role_id INT(11),
+    manager_id INT(11),
     PRIMARY KEY (id),
-    FOREIGN KEY role_id REFERENCES roles(id);
+    INDEX par_ind (role_id),
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
