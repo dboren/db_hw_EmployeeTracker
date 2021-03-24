97316e032ea8a14fdd5b54e6a65edb3b2d9563db
dboren <danielboren@att.net>
2021-03-23 22:42:09 -0500

    added mysql connection to index.js

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoletable = require("console.table");
 require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    port: 3001,
+    user: process.env.USER,
+
+    password: process.env.PASSWORD,
+    database: workforce_db,
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+  });
