53f1c01a7f805930f250227820e1300b82d6a474
dboren <danielboren@att.net>
2021-03-27 17:08:07 -0500

    added opening prompts. Need to debug dotenv issue

--- index.js
+++ index.js
@@ -4,16 +4,39 @@ const consoletable = require("console.table");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: 3001,
-    user: process.env.USER,
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
 
-    password: process.env.PASSWORD,
-    database: workforce_db,
+    password: 'Cheliax541',
+    database: 'workforce_db',
+    // insecureAuth: true
   });
   
   connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
-    connection.end();
+    init();
   });
+
+  const init = () => {
+    inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'Welcome to Workforce Manager. What would you like to do?',
+      choices: [
+        'View all employees',
+        'view employees by department',
+        'View employees by role',
+        'View employees by manager',
+        'Update employee role',
+        'Add employee',
+        'Remove employee',
+        'Add role',
+        'Add department',
+        'Exit',
+      ],
+    })
+
+};
